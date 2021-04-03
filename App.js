f324276577e7af65ac3195fcf9cbc214a7680337
osama256 <51829206+osama256@users.noreply.github.com>
2021-04-04 01:34:36 +0200

    first commit

--- App.js
+++ App.js
@@ -1,192 +1,126 @@
-import React, { useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Realm from "realm";
-// import RNFetchBlob from 'rn-fetch-blob'
-import { TextInput, Dimensions, Button, PermissionsAndroid, View, Text } from 'react-native'
+import { FlatList, TextInput, Dimensions, Button, PermissionsAndroid, View, Text } from 'react-native'
 import Form from './components/Form'
 const { width, height } = Dimensions.get('window')
-let date = new Date();
 var realm
-let task1, task2, task4, myTask
-const TaskSchema = {
-  name: "Task",
+var realmData = []
+const EmployeeSchema = {
+  name: "Employee",
   properties: {
-    _id: "int",
+    id: "int",
     name: "string",
-    status: "string?",
+    dep: "string",
   },
-  primaryKey: "_id",
+  primaryKey: "id",
 };
 export default function App() {
-  const [updateID,setUpdateID]=useState('')
-  const [updateName,setUpadteName]=useState('')
-  const [updateDep,setUpdateDep]=useState('')
-  const [deleteID,setDeleteID] = useState('')
+  const [updateID, setUpdateID] = useState('')
+  const [updateName, setUpadteName] = useState('')
+  const [updateDep, setUpdateDep] = useState('')
+  const [deleteID, setDeleteID] = useState('')
+  const [data,setData] = useState([])
   useEffect(() => {
-
-    async function getPermissions() {
-      try {
-        console.log('trying')
-        const granted = await PermissionsAndroid.requestMultiple([
-          // PermissionsAndroid.PERMISSIONS.,
-          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE],
-
-          {
-            title: "Talabat Elsokhna App Ask for Camera permission",
-            message: "We need this permission to Capture Products",
-            // "so you can take awesome pictures.",
-            buttonNeutral: "Ask Me Later",
-            buttonNegative: "Cancel",
-            buttonPositive: "OK"
-          }
-        );
-        // return granted;
-        // console.log(PermissionsAndroid.RESULTS)
-        // console.log(granted)
-        // console.log( 'results is : '+granted["android.permission.CAMERA"])
-        // for(let i in granted){
-        //     console.log(granted[i])
-        // }
-        if (granted["android.permission.CAMERA"] === PermissionsAndroid.RESULTS.GRANTED &&
-          granted["android.permission.READ_EXTERNAL_STORAGE"] === PermissionsAndroid.RESULTS.GRANTED &&
-          granted["android.permission.WRITE_EXTERNAL_STORAGE"] === PermissionsAndroid.RESULTS.GRANTED
-        ) {
-          console.log()
-        }
-      } catch (e) {
-        console.log(e)
-      }
-      //  realm = await new Realm({
-      realm = await Realm.open({
+    const newDB = async () => {
+      realm = await new Realm({
         path: "myrealm",
-        schema: [TaskSchema],
+        schema: [EmployeeSchema],
       });
     }
-    getPermissions()
-
+    newDB()
   })
   return (
     <View>
       <View style={{ flexDirection: 'row', backgroundColor: 'red', height: height / 2.2 }}>
 
         <View style={{ backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Form type="insert" />
+          <Form type="insert" cb={async (id, name, dep) => {
+            try {
+              realm.write(() => {
+                realm.create("Employee", {
+                  id,
+                  name,
+                  dep,
+                })
+              })
+            }
+            catch (e) {
+              console.log(e)
+            }
+          }} />
         </View>
         <View style={{ backgroundColor: '#d9a657', flex: 1, alignContent: 'center', justifyContent: 'center' }}>
           <Text style={{ fontSize: 16, fontWeight: 'bold', marginBottom: 20, alignSelf: 'center' }}>update</Text>
           <View style={{ flexDirection: "row", marginBottom: 10, marginHorizontal: 5 }}>
             <Text>ID </Text>
-            <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={updateID} onChangeText={(text) => {setUpdateID(text)}} keyboardType='numeric'/>
+            <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={updateID} onChangeText={(text) => { setUpdateID(text) }} keyboardType='numeric' />
           </View>
           <View style={{ flexDirection: "row", marginHorizontal: 10, marginBottom: 10, justifyContent: 'space-between' }} >
             <Text>Name </Text>
-            <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={updateName} onChangeText={(text) => {setUpadteName(text)}}/>
+            <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={updateName} onChangeText={(text) => { setUpadteName(text) }} />
           </View>
           <View style={{ marginHorizontal: 5, flexDirection: "row", marginBottom: 10, justifyContent: 'space-between', }}>
             <Text>Dep</Text>
-            <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={updateDep} onChangeText={(text) => {setUpdateDep(text)}}/>
+            <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={updateDep} onChangeText={(text) => { setUpdateDep(text) }} />
           </View>
           <View style={{ alignSelf: 'center' }}>
             <Button title="update" onPress={() => {
-
+              try {
+                realm.write(() => {
+                  realm.create("Employee", {
+                    id: parseInt(updateID, 10),
+                    name: updateName,
+                    dep: updateDep,
+                  },
+                    "modified"
+                  )
+                })
+              } catch (e) { console.log(e) }
             }} />
           </View>
         </View>
         <View style={{ backgroundColor: '#9628de', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text style={{ fontSize: 16, fontWeight: 'bold', marginBottom: 20, alignSelf: 'center' }}>delete</Text>
-          <View style={{ flexDirection: "row", marginBottom: 70, marginHorizontal: 5 }}>
+          <View style={{ flexDirection: "row", marginBottom: 105, marginHorizontal: 5 }}>
             <Text>ID </Text>
-            <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={deleteID} onChangeText={text => setDeleteID(text)} keyboardType='numeric'/>
+            <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={deleteID} onChangeText={text => setDeleteID(text)} keyboardType='numeric' />
           </View>
           <Button title="delete" onPress={() => {
-
+            try {
+              realm.write(() => {
+                let employee = realm.objectForPrimaryKey("Employee", parseInt(deleteID, 10));
+                realm.delete(employee)
+              })
+            } catch (e) { console.log(e) }
+          }}
+          />
+        </View>
+        <View style={{ width: 200, height: 29, position: 'absolute', top: 5, left: (width / 2) - 100 }}>
+          <Text style={{ alignSelf: 'center', color: '#990d0d', fontWeight: 'bold', fontSize: 20 }}>how to use Realm</Text>
+          <View style={{ height: 2, backgroundColor: '#000' }} />
+        </View>
+      </View>
+      <View style={{ height: height / 1.8, width: width,paddingTop: 20 }}>
+        <FlatList
+          data={data}
+          keyExtractor={employee => employee.id}
+          renderItem={({ item }) => {
+            return <View style={{marginHorizontal:10,marginBottom:20,backgroundColor:'#bbb'}}> 
+              <Text>ID : {item.id} </Text>
+              <Text>Name : {item.name} </Text>
+              <Text>Dep : {item.dep} </Text>
+            </View>
+          }}
+        />
+        <View style={{ position: 'absolute', bottom: 40, width: width }}>
+          <Button title="show realm" onPress={() => {
+            const employes = realm.objects("Employee");
+            setData(employes)
+            console.log(employes)
+            realmData = employes
           }} />
         </View>
-        {/* <TextInput value="fdsfsdfs"/> */}
       </View>
-      {/* <Button title="download" onPress={() => {
-        RNFetchBlob
-          .config({
-            // add this option that makes response data to be stored as a file,
-            // this is much more performant.
-            fileCache: true,
-            addAndroidDownloads: {
-              // Related to the Android only
-              useDownloadManager: true,
-              notification: false,
-              path:
-                '/storage/emulated/0/TalabatAdmin/' +
-                +
-                Math.floor(date.getTime() + date.getSeconds() / 2) + '.png'
-              ,
-              description: 'Image',
-            },
-          })
-          .fetch('GET', 'http://192.168.1.50:3000/img', {
-            //some headers ..
-          })
-          .then((res) => {
-            // the temp file path
-            console.log('The file saved to ', res.path())
-          })
-      }} />
-
-      <Button title="open database" onPress={async () => {
-
-        // console.log('hello')
-      }} />
-      <Button title='create obj' onPress={() => {
-        realm.write(() => {
-          task1 = realm.create("Task", {
-            _id: 3,
-            name: "go grocery shopping",
-            status: "Open",
-          });
-          task2 = realm.create("Task", {
-            _id: 4,
-            name: "go exercise",
-            status: "Open",
-          })
-        });
-      }} />
-      <Button title="read from realm" onPress={() => {
-        const tasks = realm.objects("Task");
-        const task = tasks.filtered("_id=3")
-        console.log(tasks)
-      }}/>
-      <Button title="update realm" onPress={() => {
-        realm.write(() => {
-          task4.status = "not Open";
-        });
-      }}/>
-      <Button title="task4" onPress={() => {
-        let taskk={
-          
-        }
-        realm.write(() => {
-          task4 = realm.create("Task",{_id:5,name:"osama",status:"closed"});
-        });
-      }}/>
-      <Button title="delete" onPress={() => {
-        let taskk={
-
-        }
-        realm.write(() => {
-          // let x={"_id": 5, "name": "osama", "status": "closed"}
-          // task1 = realm.create("Task", {
-          //   _id: 3,
-          //   name: "go grocery shopping",
-          //   status: "Open",
-          // });
-           realm.delete(myTask)
-        });
-      }}/>
-      <Button title="find one" onPress={() => {
-        myTask = realm.objectForPrimaryKey("Task", 2);
-        console.log(myTask)
-      }}/> */}
-
-      {/* <Text></Text> */}
     </View>
   )
 }
--- components/Form.js
+++ components/Form.js
@@ -1,29 +1,30 @@
 import React, { useState } from 'react'
 import { View, Text, TextInput, Button } from 'react-native'
 
-export default function Form({type,onPress}) {
+export default function Form({ type, cb }) {
+    const [id, setID] = useState('')
+    const [name, setName] = useState('')
+    const [dep, setDep] = useState('')
     return (
         <View>
-            <View style={{ backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center',width:'100%' }}>
+            <View style={{ backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%' }}>
                 <Text style={{ fontSize: 16, fontWeight: 'bold', marginBottom: 20 }}>{type}</Text>
                 <View style={{ flexDirection: "row", marginBottom: 10, marginHorizontal: 5 }}>
                     <Text>ID </Text>
-                    <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} keyboardType='numeric'/>
+                    <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} keyboardType='numeric' value={id} onChangeText={(text) => { setID(text) }} />
                 </View>
 
                 <View style={{ flexDirection: "row", marginHorizontal: 10, marginBottom: 10, justifyContent: 'space-between' }}>
                     <Text>Name </Text>
-                    <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} />
+                    <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={name} onChangeText={(text) => { setName(text) }} />
                 </View>
 
                 <View style={{ marginHorizontal: 5, flexDirection: "row", marginBottom: 10, justifyContent: 'space-between', }}>
                     <Text>Dep</Text>
-                    <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} />
+                    <TextInput style={{ flex: 1, height: 40, backgroundColor: '#bbb', marginLeft: 10, color: '#000' }} value={dep} onChangeText={(text) => { setDep(text) }} />
                 </View>
 
-                <Button title={type} onPress={() => {
-
-                }} />
+                <Button title={type} onPress={() => {cb(parseInt(id,10),name,dep)}} />
             </View>
         </View>
     )
